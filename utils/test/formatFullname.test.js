98e8e67cc22fe7bc5f12421a92be4efe35e7ebdf
Wojciech Kaczmarzyński <wojciech.kaczmarzynski@gmail.com>
2021-05-15 16:51:06 +0200

    adding unit tests

---
+++ utils/test/formatFullname.test.js
@@ -0,0 +1,22 @@
+const formatFullname = require('../formatFullname');
+const expect = require('chai').expect;
+
+describe('formatFullname', () => {
+  it('should return an error if "fullName" arg is not a string or is not provided', () => {
+    expect(formatFullname(undefined)).to.equal('Error');
+    expect(formatFullname(12)).to.equal('Error');
+    expect(formatFullname({})).to.equal('Error');
+    expect(formatFullname([])).to.equal('Error');
+    expect(formatFullname(function() {})).to.equal('Error');
+    expect(formatFullname()).to.equal('Error');
+  });
+  it('should return an error if fullName is String made of one word or more than two', () => {
+    expect(formatFullname('John Doe Second')).to.equal('Error');
+    expect(formatFullname('John')).to.equal('Error');
+  });
+  it('should correctly format "fullName" to "Name Lastname" string', () => {
+    expect(formatFullname('wojciech kacz')).to.equal('Wojciech Kacz');
+    expect(formatFullname('wOJcIech KAcZ')).to.equal('Wojciech Kacz');
+    expect(formatFullname('WojCieCh KACZ')).to.equal('Wojciech Kacz');
+  });
+});
